8223049426afed74cc67df7fb490c9405443e2bb
apachertr180navneeet <navneetgehlot03061993@gmail.com>
2025-05-20 20:42:01 +0530

    code update in node

--- hali-yatri-node/src/app.js
+++ hali-yatri-node/src/app.js
@@ -25,7 +25,7 @@ app.use((err, req, res, next) => {
 });
 
 // Set port and start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
